17a77aefecb1cab87b83c9528edda8ae31e26589
Anuar Kulmagambetov <anuar.kulmagambetov@dataart.com>
2024-03-26 13:54:49 +0500

    feat: add connecti to Db in appsettings.json

---
+++ LifeStats/LifeStats/Data/AlcoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LifeStats.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeStats.Data
+{
+    public class AlcoDbContext : DbContext
+    {
+        public AlcoDbContext(DbContextOptions dbContextOptions ): base( dbContextOptions )
+        {
+             
+        }
+
+        public DbSet<Alcohol> Alcohol { get; set; }
+
+    }
+}
