d8ed2ba957e9579314af80a51e33ed297292b9ba
Anuar Kulmagambetov <anuar.kulmagambetov@dataart.com>
2024-03-26 13:25:11 +0500

    feat: 1st domain model add

---
+++ LifeStats/LifeStats/Controllers/AlcoholController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeStats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlcoholController : ControllerBase
+    {
+    }
+}
---
+++ LifeStats/LifeStats/Models/Domain/Alcohol.cs
@@ -0,0 +1,10 @@
+﻿namespace LifeStats.Models.Domain
+{
+    public class Alcohol
+    {
+        public Guid Id { get; set; }
+        public string Name {  get; set; }
+        public double Percentage { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
